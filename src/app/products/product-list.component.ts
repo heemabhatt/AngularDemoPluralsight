b3373017f34d835218d89f56dab89c9a687e4ab7
heemabhatt <heema.bhatt@gmail.com>
2019-01-11 17:19:41 -0500

    module5

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,30 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import {StarComponent} from '../shared/star.component';
 @Component({
     selector:'pm-products',
-    templateUrl:'./product-list.component.html'
+    templateUrl:'./product-list.component.html',
+    styleUrls:['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
+    onNotify(message:string):void{}
     pageTitle:string ='Product List';
     imageWidth:number=50;
     imageMargin:number=2;
     showImage:boolean=false;
-    listFilter:string='cart';
-    products:any[]=[
+    _listFilter:string;
+    get listFilter():string{
+        return this._listFilter;
+    }
+    set listFilter(value:string)
+    {
+        this._listFilter=value;
+        this.filteredProducts = this.listFilter? this.performFilter(this.listFilter) :this.products;
+    }
+
+    filteredProducts:IProduct[];
+    products:IProduct[]=[
             {
               "productId": 1,
               "productName": "Leaf Rake",
@@ -30,10 +44,60 @@ export class ProductListComponent{
               "price": 32.99,
               "starRating": 4.2,
               "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+            },
+            {
+              "productId": 5,
+              "productName": "Hammer",
+              "productCode": "TBX-0048",
+              "releaseDate": "May 21, 2016",
+              "description": "Curved claw steel hammer",
+              "price": 8.9,
+              "starRating": 4.8,
+              "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+            },
+            {
+              "productId": 8,
+              "productName": "Saw",
+              "productCode": "TBX-0022",
+              "releaseDate": "May 15, 2016",
+              "description": "15-inch steel blade hand saw",
+              "price": 11.55,
+              "starRating": 3.7,
+              "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+            },
+            {
+              "productId": 10,
+              "productName": "Video Game Controller",
+              "productCode": "GMG-0042",
+              "releaseDate": "October 15, 2015",
+              "description": "Standard two-button video game controller",
+              "price": 35.95,
+              "starRating": 4.6,
+              "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
             }
     ];
 
+    constructor(){
+        this.filteredProducts=this.products;
+        this.listFilter='cart';
+    }
+
+    onRatingClicked(message:string) : void{
+        this.pageTitle = 'Product List: ' + message;
+    }
+
+    performFilter(filterBy:string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product:IProduct)=>
+                product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage():void{
         this.showImage= !this.showImage;
+    }
+
+    ngOnInit():void{
+
+        console.log("In OnInit");
     }
 }
