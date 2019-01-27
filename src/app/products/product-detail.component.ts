22e084742e3b1f49e967885d134ba53d9171f09c
heemabhatt <heema.bhatt@gmail.com>
2019-01-27 15:25:04 -0500

    Complete till module Navigation and Routing

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {IProduct} from './product';
+import {Router, ActivatedRoute} from '@angular/router';
+import { ProductService } from './product.service';
+
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle:string ='Product Detail';
+  errorMessage='';
+  product: IProduct;
+
+  constructor(private route:ActivatedRoute, 
+              private router:Router,
+              private productService: ProductService) { 
+   
+  }
+
+  ngOnInit() {
+    const param = this.route.snapshot.paramMap.get('id');
+    if(param){
+      const id = +param;
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id:number){
+    this.productService.getProduct(id).subscribe(
+      product=>this.product=product,
+      error=> this.errorMessage= <any>error);
+  }
+
+  onBack():void{
+    this.router.navigate(['/products']);
+  }
+
+}
